0d5e2933b2a5ee6cb1567cdcdb4b6f68c9dd4f45
Nick Amoscato <nick.amoscato@jazz.co>
2018-01-29 09:24:24 -0500

    Fix swimlane header expander style issue

--- src/issue-status-width-fix.js
+++
@@ -1,9 +0,0 @@
-(() => {
-    setTimeout(() => {
-        const issueStatuses = document.querySelectorAll('.ghx-agile .aui-lozenge');
-        
-        for (let issueStatus of issueStatuses) {
-            issueStatus.style.maxWidth = 'initial';
-        }
-    }, 1000);
-})();
